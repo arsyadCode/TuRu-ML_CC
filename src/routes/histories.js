4de8668760a33f0143a966d837fd00f7fb296f2f
Mgs. Tabrani <mgstabrani19@gmail.com>
2022-06-10 17:20:59 +0700

    feature-fix: remove authentication on get all histories (#34)

--- src/routes/histories.js
+++ src/routes/histories.js
@@ -1,7 +1,7 @@
 module.exports = function historiesRouter(express, verifyToken, historiesController) {
   const router = express.Router();
 
-  router.get('/', verifyToken, historiesController.getAllHistories);
+  router.get('/', historiesController.getAllHistories);
   router.get('/:id', verifyToken, historiesController.getHistoryById);
   router.get('/users/:id', verifyToken, historiesController.getHistoriesByUserId);
   router.post('/', verifyToken, historiesController.createHistory);
