4b23beb1b13c4922ca67a1f8b3d7734eece7eea2
Mgs. Tabrani <mgstabrani19@gmail.com>
2022-06-10 17:17:51 +0700

    feature-fix: remove authentication on get all bookmarks (#33)

--- src/routes/bookmarks.js
+++ src/routes/bookmarks.js
@@ -1,7 +1,7 @@
 module.exports = function bookmarksRouter(express, verifyToken, bookmarksController) {
   const router = express.Router();
 
-  router.get('/', verifyToken, bookmarksController.getAllBookmarks);
+  router.get('/', bookmarksController.getAllBookmarks);
   router.get('/:id', verifyToken, bookmarksController.getBookmarkById);
   router.get('/users/:id', verifyToken, bookmarksController.getBookmarksByUserId);
   router.post('/', verifyToken, bookmarksController.createBookmark);
